c202952c21377d56022abed3497aea9357d4b861
Swagat Konchada <skonchada@bloomberg.net>
2023-02-26 13:24:36 +0000

    shortcut gpp to compile and run c++ files

---
+++ functions/gpp.fish
@@ -0,0 +1,15 @@
+function gpp
+#    echo $argv
+#    echo $argv[1]
+    if string match -q --regex ".cpp" $argv[1]
+#	echo "inside first if"
+        set executable (string replace ".cpp" ".tsk" $argv[1])
+    end
+    if string match -q --regex ".hpp" $argv[1]
+#	echo "inside second if"
+        set executable (string replace ".hpp" ".tsk" $argv[1])
+    end
+    echo $executable
+    g++ -I ~/Downloads/boost_1_81_0 -Wno-c++11-extensions $argv[1] -o $executable
+    ./$executable
+end
